cc622a8fdebc92ab32460cb061f7e613008d3cae
Ahmed Nasser <ahmed.nasser.ali+zse@zalando.de>
2025-11-30 15:11:26 +0100

    test

--- Package.swift
+++ Package.swift
@@ -4,21 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "LoungeTrackingDefinitions",
+    name: "TestFramework",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "LoungeTrackingDefinitions",
-            targets: ["LoungeTrackingDefinitions"]),
+            name: "TestFramework",
+            targets: ["TestFramework"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
-            name: "LoungeTrackingDefinitions",
-            url: "https://github.com/AhmedNasserSh/MyFrameWork/releases/download/1.0.1/LoungeTrackingDefinitions.xcframework.zip",
-            checksum: "754491c2fe482fbf83ac0011c7adb48be3ec079843b8ce6b557bb1d8b3329811"
+            name: "TestFramework",
+            url: "https://github.com/AhmedNasserSh/TestFrameWork/releases/download/1.0.0/MyFramework.xcframework.zip",
+            checksum: "bd99dac21b7f0e6ae049c66e60506cf75bcc7fd9ee7d85a1243c60a0276194b3"
         )
     ]
 )
-
